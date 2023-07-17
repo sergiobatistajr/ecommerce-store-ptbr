ebff065062e95ad4293dc1e3238991b35def6a4c
surfacez <sergiobatistajr94@gmail.com>
2023-07-17 11:22:44 -0300

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("c4e86acc-eafd-45f6-80e0-69656245a697");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
